a17f748ba495af926972ceabf75d07a29ac5e217
rickyDefianS <rickydefians@gmail.com>
2022-08-18 22:24:34 +0700

    deployed server done, prepare deploy client side

--- client/src/store/actions/actionType.js
+++ client/src/store/actions/actionType.js
@@ -1,3 +1,3 @@
-export const BASE_URL = "http://localhost:3000";
+export const BASE_URL = "https://myhon-service-app.herokuapp.com";
 export const FETCH_FAVORITE_BOOKS = "favoriteBooks/fetchData";
 export const FETCH_BOOKS = "books/fetchData";
